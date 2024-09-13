257fe5e00fc0dcf5dc011fc729d5dc6722cb39e0
svetlana <s.tarasenko.it@gmail.com>
2024-09-13 23:41:45 +0300

    task 12

---
+++ src/main/java/ooap2/task12/Exercise.java
@@ -0,0 +1,26 @@
+package ooap2.task12;
+
+import java.io.Serializable;
+
+class General implements Serializable {
+
+    static General assignmentAttempt(General target, General source) {
+        if (target.getClass().isAssignableFrom(source.getClass())) {
+            return source;
+        }
+        return new Void();
+    }
+}
+
+class Any extends General {
+
+    static Any assignmentAttempt(Any target, Any source) {
+        if (target.getClass().isAssignableFrom(source.getClass())) {
+            return source;
+        }
+        return new Void();
+    }
+}
+
+final class Void extends Any /*A, B, ....*/ {
+}
